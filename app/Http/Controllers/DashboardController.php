e156e799c9823b30733ab157143ed64449f2c3fa
Tuấn Lê <n19dccn177@student.ptithcm.edu.vn>
2024-03-22 22:40:03 +0700

    feat: search

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,8 +8,13 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $all_ideas = Ideas::orderBy("created_at", "DESC")->paginate(2);
+        $all_ideas = Ideas::orderBy("created_at", "DESC");
 
-        return view("dashboard", ["ideas" => $all_ideas]);
+        if (request()->has("search")) {
+            $search_term = request()->get('search', '');
+            $all_ideas = $all_ideas->where('content', 'like', '%' . $search_term);
+        }
+
+        return view("dashboard", ["ideas" => $all_ideas->paginate(2)]);
     }
 }
